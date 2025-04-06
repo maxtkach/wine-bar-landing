17d8fd134f5678d912b36e5626ed504211bc74f9
Max Tkach <maxtkach4422@gmail.com>
2025-04-06 21:28:09 +0300

    гоу

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   content: [
     './src/**/*.{js,ts,jsx,tsx}',
+    './src/styles/**/*.{css,scss}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: {
+          DEFAULT: '#FFD700',
+        },
+      },
+    },
   },
   plugins: [],
 }; 
